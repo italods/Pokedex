1a356c21a4465907346a2852550537b7101dc055
Italo Pereira <italopereira007@hotmail.com>
2021-01-31 21:22:06 -0300

    Subindo projeto final

--- script.js
+++ script.js
@@ -15,21 +15,62 @@ const types = [
   'normal',
 ];
 
-const POKEMON_COUNT = 12;
+const POKEMON_COUNT = 16;
+
+const cardHTML = `
+<div class="card" id="card-{id}">
+<div class="title">
+  <h2>{name}</h2>
+  <small># {id}</small>
+</div>
+<div class="img bg-{type}">
+  <img src="https://pokeres.bastionbot.org/images/pokemon/{id}.png" alt="{name}"/>
+</div>
+<div class="type {type}">
+  <p>{type}</p>
+</div>
+<button class="favorite" data-id={id}>
+  <div class="heart"></div>
+</button>
+</div>
+`;
+
+const cards = document.querySelector(".cards");
 
 const getType = (data) => {
-  const apiType = data.map(type => type.name);
-  console.log(apiType);  
-}
+  const apiTypes = data.map((type) => type.type.name);
+  const type = types.find((type) => apiTypes.indexOf(type) > -1);
+  return type;
+};
 
 const fetchPokemon = async (number) => {
+  if (number === undefined) return;
   const url = `https://pokeapi.co/api/v2/pokemon/${number}`; 
   const response = await fetch(url).then((response) => response.json());
-  const {id, name, types } = response;
+  const { id, name, types } = response;
   const type = getType(types);
+  return { id, name, type };
+};
 
-  console.log(id, name, types);
-  console.log(response);
-}
+const replacer = (text, source, destination) => {
+  const regex = new RegExp(source, 'gi');
+  return text.replace(regex, destination);
+};
 
-fetchPokemon(1);
+const createPokemonCard = (pokemon) => {
+  const { id, name, type } = pokemon;
+  let newCard = replacer(cardHTML, `\{id\}`, id);
+  newCard = replacer(newCard, `\{name\}`, name);
+  newCard = replacer(newCard, `\{type\}`, type);
+
+  cards.innerHTML += newCard;
+};
+
+const fetchPokemons = async () => {
+  for (let i =1; i <= POKEMON_COUNT; i++){
+    const pokemon = await fetchPokemon(i);
+    createPokemonCard(pokemon);
+  }
+};
+
+fetchPokemons();
